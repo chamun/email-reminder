3f5708b5630f6a098e91ba1a126655a46315ebed
Rodrigo Chamun <rochamun@gmail.com>
2016-10-30 20:45:32 -0200

    Finish missing title path

--- spec/features/new_reminder_spec.rb
+++ spec/features/new_reminder_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Creating a reminder' do
 
-  scenario 'Submiting the form' do
+  scenario 'Submiting a complete form' do
     visit new_reminder_path
     fill_in 'Title', with: 'Eggs'
     fill_in 'Date', with: '12/20/1991'
@@ -12,4 +12,15 @@ RSpec.feature 'Creating a reminder' do
 
     expect(page).to have_content('Reminder successfully created!')
   end
+
+  scenario 'Submiting the form without a title' do
+    visit new_reminder_path
+    fill_in 'Date', with: '12/20/1991'
+    select '01:00 PM', from: 'Time'
+    fill_in 'Message', with: "Don't forget to buy eggs"
+    click_on 'Submit'
+
+    expect(page).to have_content('Reminder could not be created!')
+  end
+
 end
