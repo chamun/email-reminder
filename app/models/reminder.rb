64cf10aca246e10a8c0de2ae073198bded5757c5
Rodrigo Chamun <rochamun@gmail.com>
2016-11-04 21:52:37 -0200

    Validates reminder time numericality

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,10 @@
 class Reminder < ApplicationRecord
   validates :title, presence: true
+  validates :time, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than: 24
+  }
   validate :current_date
 
   private
--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Reminder, type: :model do
 
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:date) }
+  it { is_expected.to validate_numericality_of(:time)
+                        .is_greater_than_or_equal_to(0)
+                        .is_less_than(24)
+                        .only_integer }
 
   it 'validates current date' do
     reminder = build(:reminder, date: Date.yesterday)
