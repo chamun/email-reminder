15e99e77e9cd2a131136fe85016dce1874bf27cc
Rodrigo Chamun <rochamun@gmail.com>
2016-11-04 22:15:17 -0200

    Save time value filled in the form

--- app/helpers/reminders_helper.rb
+++ app/helpers/reminders_helper.rb
@@ -1,9 +1,19 @@
 module RemindersHelper
   def time_options
-    %w(AM PM).collect do |period|
-      (1..11).collect do |hour|
-        "#{sprintf '%02d', hour}:00 #{period}"
-      end.unshift("12:00 #{period}")
-    end.flatten
+    (1..23).collect do |hour|
+      [ to_am_pm(hour), hour ]
+    end.unshift(['12:00 AM', 0])
+  end
+
+  private
+
+  def to_am_pm(hour)
+    result = hour % 12
+    result = 12 if result == 0
+    "#{ sprintf '%02d', result }:00 #{ period(hour) }"
+  end
+
+  def period(hour)
+    hour < 12 ? 'AM' : 'PM'
   end
 end
