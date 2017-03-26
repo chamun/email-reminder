7dde4478b8d380ea48de90b3c6f92f25a472f98b
Rodrigo Chamun <rochamun@gmail.com>
2017-03-26 17:06:40 -0300

    Send the reminder's message in the email body

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,5 +1,6 @@
 class ReminderMailer < ApplicationMailer
   def remind(reminder)
+    @reminder = reminder
     mail(
       to: reminder.user_email,
       subject: reminder.title
--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -12,5 +12,9 @@ RSpec.describe ReminderMailer, type: :mailer do
     it "sets the reminder's user's email as the recipient" do
       expect(mail.to).to eq([reminder.user_email])
     end
+
+    it "sets the reminder's message as the body" do
+      expect(mail.body.encoded).to have_text(reminder.message)
+    end
   end
 end
