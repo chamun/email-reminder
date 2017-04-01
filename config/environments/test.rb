b52fb3d914baca353a1372100f0938eeb08afec1
Rodrigo Chamun <rochamun@gmail.com>
2017-04-01 18:42:31 -0300

    Install Devise

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,6 +21,10 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.action_mailer.default_url_options = {
+    host: 'www.example.com',
+    port: 80
+  }
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
