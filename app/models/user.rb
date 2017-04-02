2981002c0377e381af50e50a072682037ec2f529
Rodrigo Chamun <rochamun@gmail.com>
2017-04-02 17:58:34 -0300

    Destroy reminders when a user is destroyed
    Devise adds the feature to destroy users. To keep the database
    consistent, when a user is destroyed her reminders are destroyed as
    well.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable
 
-  has_many :reminders
+  has_many :reminders, dependent: :destroy
 
   validates :email, uniqueness: true, presence: true
 end
