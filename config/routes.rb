1b22ee4509730e714c73fdaa074856a2ab0339c0
Rodrigo Chamun <rochamun@gmail.com>
2017-04-10 22:40:21 -0300

    Set up root URLs
    If the user is logged in, the root URL is the new reminders page,
    otherwise it is the log in page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated(:user) { root "reminders#new" }
+
+  unauthenticated do
+    devise_scope(:user) { root "devise/sessions#new" }
+  end
 
   resources :reminders, only: [:new, :create]
 end
